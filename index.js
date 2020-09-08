59483cff2dbfdec6b016b7c311e8999d671061d8
Marvel Dev <marvellousalika48@gmail.com>
2020-09-08 18:30:49 +0100

    - add background-color to body. - add height 100%.

--- index.js
+++ index.js
@@ -41,12 +41,15 @@ tweetModalOverlay.addEventListener('click', () => {
 
 dimThemeButton.addEventListener('click', () => {
   root.className = 'dim-theme';
+  document.querySelector('body').style.backgroundColor = '#15202B';
 })
 
 defaultThemeButton.addEventListener('click', () => {
   root.className = 'root';
+  document.querySelector('body').style.backgroundColor = '#fff';
 })
 
 darkThemeButton.addEventListener('click', () => {
   root.className = 'dark-theme';
+  document.querySelector('body').style.backgroundColor = '#000';
 })
