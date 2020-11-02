4ef588d0eeb11e19d6bf1c6530f14cb65b5cceb6
Marvel Dev <marvellousalika48@gmail.com>
2020-11-02 17:09:17 +0100

    - add profile photo events. -add profile names event

--- Modules/nav/events.js
+++ Modules/nav/events.js
@@ -1,4 +1,4 @@
-import { addEntryToDb } from '../../dataStorage.js';
+import { addEntryToDb, clearAllEntries } from '../../dataStorage.js';
 
 const addProfileEventListeners = () => {
   const photoInput = document.querySelector('#profilePhoto');
@@ -16,22 +16,23 @@ const addProfileEventListeners = () => {
     event.preventDefault();
     const profileName = document.querySelector('#profileInput').value;
     const photoSource = profilePhoto.src
-    // document.querySelector('.edit-profile-modal').style.display = 'none';
-    // document.querySelector('#dropdown-overlay').style.display = 'none';
-    
-    // const profilePhotos = document.querySelectorAll('.profile-photo')
-    // for (let index = 0; index < profilePhotos.length; index++) {
-    //   const profilePhoto = profilePhotos[index];
-    //   profilePhoto.src = photoSource
-    // }
-    
-    // const userNames = document.querySelectorAll('.user-name')
-    // for (let index = 0; index < userNames.length; index++) {
-    //   const userName = userNames[index];
-    //   userName.innerText = profileName;  
-    // }
+  
+    const profilePhotos = document.querySelectorAll('.image')
+    for (let index = 0; index < profilePhotos.length; index++) {
+      const profilePhoto = profilePhotos[index];
+      profilePhoto.src = photoSource
+    }
+  
+    const names = document.querySelectorAll('.profile-name')
+    for (let index = 0; index < names.length; index++) {
+      const name = names[index];
+      name.innerText = profileName;
+    }
 
-    // clearAllEntries('profile');
+    document.querySelector('.edit-profile-modal').style.display = 'none';
+    document.querySelector('#tweet-modal-overlay').style.display = 'none';
+
+    clearAllEntries('profile');
     addEntryToDb('profile', { profileName, photoSource })
   })
 }
