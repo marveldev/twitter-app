4c17bfeb1a39d703a7a9bdee6289849617d40c8a
Marvel Dev <marvellousalika48@gmail.com>
2020-11-06 06:50:12 +0100

    - add event listener

--- Modules/smallScreen/smalScreen.js
+++ Modules/smallScreen/smalScreen.js
@@ -1,33 +1,33 @@
-// const smallScreenEventListeners = () => {
-//   const tweetModalOverlay = document.querySelector('#tweet-modal-overlay');
-//   const displayThemeButton = document.querySelector('#display-button');
-//   const dropdownOverlay = document.querySelector('#dropdown-overlay');
-//   const smallScreenTrends = document.querySelector('#smallscreen-trend');
-//   const smallScreenModal = document.querySelector('#smallscreen-modal');
-//   const smallScreenTheme = document.querySelector('.small-screen-photo');
-//   const tweetModalIcon = document.querySelector('#tweet-modal-icon');
-//   const smallScreenHome = document.querySelector('#smallscreen-home');
+const smallScreenEventListeners = () => {
+  const tweetModalOverlay = document.querySelector('#tweet-modal-overlay');
+  const displayThemeButton = document.querySelector('#display-button');
+  const dropdownOverlay = document.querySelector('#dropdown-overlay');
+  const smallScreenTrends = document.querySelector('#smallscreen-trend');
+  const smallScreenModal = document.querySelector('#smallscreen-modal');
+  const smallScreenTheme = document.querySelector('.small-screen-photo');
+  const tweetModalIcon = document.querySelector('#tweet-modal-icon');
+  const smallScreenHome = document.querySelector('#smallscreen-home');
 
-//   function changeDisplay(element, selector, value, overlay) {
-//     element.addEventListener('click', () => {
-//       document.querySelector(selector).style.display = value;
-//       overlay.style.display = value;
-//     })
-//   }
+  function changeDisplay(element, selector, value, overlay) {
+    element.addEventListener('click', () => {
+      document.querySelector(selector).style.display = value;
+      overlay.style.display = value;
+    })
+  }
 
-//   changeDisplay(tweetModalIcon, '.tweet-modal-container', 'block', tweetModalOverlay)
-//   changeDisplay(smallScreenModal, '.tweet-modal-container', 'block', tweetModalOverlay);
-//   changeDisplay(smallScreenTheme, '.dropdown-content', 'block', dropdownOverlay);
+  changeDisplay(tweetModalIcon, '.tweet-modal-container', 'block', tweetModalOverlay)
+  changeDisplay(smallScreenModal, '.tweet-modal-container', 'block', tweetModalOverlay);
+  changeDisplay(smallScreenTheme, '.dropdown-content', 'block', dropdownOverlay);
 
-//   smallScreenTrends.addEventListener('click', () => {
-//     document.querySelector('.right-side-nav').style.display = 'block';
-//     document.querySelector('#tweet-data').style.display = 'none';
-//   })
+  smallScreenTrends.addEventListener('click', () => {
+    document.querySelector('.right-side-nav').style.display = 'block';
+    document.querySelector('#tweet-data').style.display = 'none';
+  })
 
-//   smallScreenHome.addEventListener('click', () => {
-//     document.querySelector('.right-side-nav').style.display = 'none';
-//     document.querySelector('#tweet-data').style.display = 'block';
-//   })
-// }
+  smallScreenHome.addEventListener('click', () => {
+    document.querySelector('.right-side-nav').style.display = 'none';
+    document.querySelector('#tweet-data').style.display = 'block';
+  })
+}
 
-// export default smallScreenEventListeners
+export default smallScreenEventListeners
