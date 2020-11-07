4f554bd94aae83cbf8b6a7cd4d7f1b5accf69044
Marvel Dev <marvellousalika48@gmail.com>
2020-11-07 05:23:13 +0100

    - add profile button event listener

--- Modules/smallScreen/smalScreen.js
+++ Modules/smallScreen/smalScreen.js
@@ -1,10 +1,10 @@
 const smallScreenEventListeners = () => {
   const tweetModalOverlay = document.querySelector('#tweet-modal-overlay');
-  const displayThemeButton = document.querySelector('#display-button');
-  const dropdownOverlay = document.querySelector('#dropdown-overlay');
+  const displayThemeButton = document.querySelector('#displayButton');
+  const profileButton = document.querySelector('#profile-button');
   const smallScreenTrends = document.querySelector('#smallscreen-trend');
   const smallScreenModal = document.querySelector('#smallscreen-modal');
-  const smallScreenTheme = document.querySelector('.small-screen-photo');
+  const smallScreenPhoto = document.querySelector('.small-screen-photo');
   const tweetModalIcon = document.querySelector('#tweet-modal-icon');
   const smallScreenHome = document.querySelector('#smallscreen-home');
 
@@ -15,18 +15,30 @@ const smallScreenEventListeners = () => {
     })
   }
 
-  changeDisplay(tweetModalIcon, '.tweet-modal-container', 'block', tweetModalOverlay)
+  // changeDisplay(tweetModalIcon, '.tweet-modal-container', 'block', tweetModalOverlay)
   changeDisplay(smallScreenModal, '.tweet-modal-container', 'block', tweetModalOverlay);
-  changeDisplay(smallScreenTheme, '.dropdown-content', 'block', dropdownOverlay);
+  changeDisplay(smallScreenPhoto, '.smallscreen-left-nav', 'block', tweetModalOverlay);
+  changeDisplay(tweetModalOverlay, '.smallscreen-left-nav', 'none', tweetModalOverlay);
+
 
   smallScreenTrends.addEventListener('click', () => {
     document.querySelector('.right-side-nav').style.display = 'block';
-    document.querySelector('#tweet-data').style.display = 'none';
+    document.querySelector('#tweet-output').style.display = 'none';
   })
 
   smallScreenHome.addEventListener('click', () => {
     document.querySelector('.right-side-nav').style.display = 'none';
-    document.querySelector('#tweet-data').style.display = 'block';
+    document.querySelector('#tweet-output').style.display = 'block';
+  })
+
+  profileButton.addEventListener('click', () => {
+    document.querySelector('.edit-profile-modal').style.display = 'block';
+    document.querySelector('.smallscreen-left-nav').style.display = 'none';
+  })
+
+  displayThemeButton.addEventListener('click', () => {
+    document.querySelector('.theme-modal').style.display = 'block';
+    document.querySelector('.smallscreen-left-nav').style.display = 'none';
   })
 }
 
