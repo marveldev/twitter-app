85e64919ab405f0b6abb545f2c777c72f40f7be0
Marvel Dev <marvellousalika48@gmail.com>
2020-11-08 08:20:03 +0100

    - rename theme

--- theme/theme.js
+++ theme/theme.js
@@ -1,30 +1,30 @@
 const toggleTheme = () => {
   const root = document.querySelector('#root');
-  const dimThemeButton = document.querySelector('#dim-button');
-  const defaultThemeButton = document.querySelector('#default-button');
-  const darkThemeButton = document.querySelector('#light-out-button');
+  const dimButton = document.querySelector('#dim-button');
+  const defaultButton = document.querySelector('#default-button');
+  const lightsOutButton = document.querySelector('#light-out-button');
 
   root.className = localStorage.getItem('theme');
-  document.querySelector('body').style.backgroundColor = localStorage.getItem('background-color') ;
+  document.querySelector('body').style.backgroundColor = localStorage.getItem('background-color');
 
-  defaultThemeButton.addEventListener('click', () => {
-    root.className = 'root';
+  defaultButton.addEventListener('click', () => {
+    root.className = 'default';
     document.querySelector('body').style.backgroundColor = '#fff';
-    localStorage.setItem('theme', 'root')
+    localStorage.setItem('theme', 'default')
     localStorage.setItem('background-color', '#fff')
   })
 
-  dimThemeButton.addEventListener('click', () => {
-    root.className = 'dim-theme';
+  dimButton.addEventListener('click', () => {
+    root.className = 'dim';
     document.querySelector('body').style.backgroundColor = '#15202B';
-    localStorage.setItem('theme', 'dim-theme')
+    localStorage.setItem('theme', 'dim')
     localStorage.setItem('background-color', '#15202b')
   })
 
-  darkThemeButton.addEventListener('click', () => {
-    root.className = 'dark-theme';
+  lightsOutButton.addEventListener('click', () => {
+    root.className = 'lights-out';
     document.querySelector('body').style.backgroundColor = '#000';
-    localStorage.setItem('theme', 'dark-theme')
+    localStorage.setItem('theme', 'lights-out')
     localStorage.setItem('background-color', '#000')
   })
 }
