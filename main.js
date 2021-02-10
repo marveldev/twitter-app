781d7d1400f661fcdc7b37abac8688a4ea12041f
Marvel Dev <marvellousalika48@gmail.com>
2021-02-10 07:33:32 +0100

    refactor files

--- main.js
+++ main.js
@@ -2,12 +2,12 @@ import modal from './Modules/modal/Modal.js';
 import LeftNav from './Modules/leftNav/LeftNav.js';
 import MainContent from './Modules/MainContent/MainContent.js';
 import TrendingPane from './Modules/trendingPane/TrendingPane.js';
-import toggleTheme from './theme/theme.js';
 import addModalEventListeners from './Modules/modal/events.js';
 import { request } from './dataStorage.js';
 import { addProfileEventListeners } from './Modules/leftNav/events.js';
 import { addTweetItemToDb, getTweetItemFromDb } from './Modules/MainContent/events.js';
 import smallScreenEventListeners from './Modules/smallScreen/events.js';
+import toggleTheme from "./theme.js";
 
 const mainApp = async () => {
   return `
@@ -22,7 +22,7 @@ const mainApp = async () => {
 
 request.onsuccess = async () => {
   document.getElementById('root').innerHTML = await mainApp();
-  toggleTheme();
+  toggleTheme()
   addModalEventListeners();
   addProfileEventListeners();
   addTweetItemToDb();
