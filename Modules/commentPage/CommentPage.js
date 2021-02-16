6fa950234cc9ffc01d3853462ebbaf8389d23142
Marvel Dev <marvellousalika48@gmail.com>
2021-02-16 08:08:32 +0100

    refactor comment-info div items

--- Modules/commentPage/CommentPage.js
+++ Modules/commentPage/CommentPage.js
@@ -6,19 +6,19 @@ const CommentPage = () => {
         <span>Tweet</span>
       </div>
       <div class="comment-info">
-        <div class="profile" id="">
+        <div>
           <img src="https://history.ucr.edu/sites/g/files/rcwecm1916/files/styles/form_preview/public/blank-profile-picture-png.png?itok=MQ-iPuNG" class="main-content-photo image" alt="photo">
           <strong class="profile-name">Jane Doe</strong>
           <button class="delete-button" property=""><i class="material-icons">&#xe5d3;</i></button>
         </div>
-        <div class="comment-text">
-          <p class="tweet-text">Hey</p>
+        <div>
+          <p class="comment-text">Hey</p>
           <p>2:51pm. Feb 15, 2021. Twitter Web App</p>
         </div>
-        <div class="tweet-info">
-          <button class="comment-button" property=""><i class="fa fa-comment-o"></i>5.1k</button>
-          <button><i class="fa fa-retweet"></i>2.1k</button>
-          <button><i class="fa fa-heart-o"></i>3.1k</button>
+        <div class="comment-options">
+          <button class="comment-button"><i class="fa fa-comment-o"></i></button>
+          <button><i class="fa fa-retweet"></i></button>
+          <button><i class="fa fa-heart-o"></i></button>
           <button><i class="fa fa-upload"></i></button>
         </div>
       </div>
