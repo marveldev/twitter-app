2fc5a2501ca720b54192750f1ac225825fe53d5c
Marvel Dev <marvellousalika48@gmail.com>
2021-02-17 07:46:21 +0100

    initial commit

---
+++ Modules/helper.js
@@ -0,0 +1,25 @@
+import CommentPage from "./commentPage/CommentPage.js"
+import addCommentPageEvents from "./commentPage/events.js"
+import addMainContentEvents from "./mainContent/events.js"
+import MainContent from "./MainContent/MainContent.js"
+
+const switchCurrentPage = async (page) => {
+  const currentPage = document.querySelector('#currentPage')
+  localStorage.setItem('currentPage', page)
+
+  switch (page) {
+    case 'homePage':
+      currentPage.innerHTML = await MainContent()
+      addMainContentEvents()
+      break
+    case 'commentPage':
+      currentPage.innerHTML = CommentPage()
+      addCommentPageEvents()
+      break
+    default:
+      currentPage.innerHTML = MainContent()
+      break
+  }
+}
+
+export default switchCurrentPage
