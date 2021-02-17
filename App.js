7d9fe63770fad3dae9008f92356d79464b719843
Marvel Dev <marvellousalika48@gmail.com>
2021-02-17 12:18:45 +0100

    remove default currentPage innerHTML

--- App.js
+++ App.js
@@ -1,10 +1,9 @@
 import LeftNav from './Modules/leftNav/LeftNav.js'
-import MainContent from './Modules/MainContent/MainContent.js'
 import TrendingPane from './Modules/trendingPane/TrendingPane.js'
 import { request } from './dataStorage.js'
 import addLeftNavEventListeners from './Modules/leftNav/events.js'
-import addMainContentEvents from './Modules/MainContent/events.js'
 import addTrendingPaneEvents from './Modules/trendingPane/events.js'
+import switchCurrentPage from './Modules/helper.js'
 
 const App = async () => {
   return `
@@ -12,7 +11,6 @@ const App = async () => {
     ${await LeftNav()}
     <section class="grid-container">
       <div id="currentPage">
-        ${await MainContent()}
       </div>
       ${TrendingPane()}
     </section>
@@ -22,8 +20,8 @@ const App = async () => {
 request.onsuccess = async () => {
   document.getElementById('root').innerHTML = await App()
   const currentPage = localStorage.getItem('currentPage')
+  switchCurrentPage(currentPage)
 
   addLeftNavEventListeners()
-  addMainContentEvents()
   addTrendingPaneEvents()
 }
