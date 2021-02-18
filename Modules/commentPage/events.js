1ea75ac42be72d7e7b7e1500fe260f0d0b55fb34
Marvel Dev <marvellousalika48@gmail.com>
2021-02-18 12:20:55 +0100

    rename names

--- Modules/commentPage/events.js
+++ Modules/commentPage/events.js
@@ -1,23 +1,23 @@
 import { addEntryToDb, getEntryFromDb } from "../../dataStorage.js"
 import switchCurrentPage from "../helper.js"
 
-const addreplyItemToDb = () => {
-  const replyOutput = document.querySelector('#replyOutput')
+const addcommentItemToDb = tweetId => {
+  const commentOutput = document.querySelector('#commentOutput')
   const commentBox = document.querySelector('.comment-box')
 
   if (commentBox.value.trim().length >= 1) {
-    const replyItemId = 'id' + Date.parse(new Date()).toString()
+    const commentItemId = 'id' + Date.parse(new Date()).toString()
     const userName = document.querySelector('.profile-name').innerText
     const userPhoto = document.querySelector('.left-nav-photo').src
     const commentBoxValue = commentBox.value
-    let replyItem = `
-      <div class="reply-item" id="${replyItemId}">
-        <div class="reply-content-item">
+    let commentItem = `
+      <div class="comment-item" id="${commentItemId}">
+        <div class="comment-content-item">
           <img src="${userPhoto}" class="home-page-photo image" alt="photo">
           <div>
             <strong class="user-profile-name">${userName}</strong>
             <p>${commentBoxValue}</p>
-            <div class="reply-item-options">
+            <div class="comment-item-options">
               <button><i class="fa fa-comment-o"></i></button>
               <button><i class="fa fa-retweet"></i></button>
               <button><i class="fa fa-heart-o"></i></button>
@@ -28,37 +28,35 @@ const addreplyItemToDb = () => {
         <button class="more-button" property=""><i class="material-icons">&#xe5d3;</i></button>
       </div>
     `
-    replyItem += replyOutput.innerHTML
-    replyOutput.innerHTML = replyItem
+    commentItem += commentOutput.innerHTML
+    commentOutput.innerHTML = commentItem
 
     commentBox.value = ''
 
     document.querySelector('.comment-modal-container').style.display = 'none'
     document.querySelector('#overlay').style.display = 'none'
 
-    const addItemToIndexDb = {
-      replyItemId: replyItemId,
-      userName: userName,
-      userPhoto: userPhoto,
-      commentBoxValue: commentBoxValue
-    }
+    // const commentObject = {
+    //   tweetId: tweetId,
+    //   text: commentBoxValue
+    // }
 
-    addEntryToDb('reply-data', addItemToIndexDb)
+    // addEntryToDb('comment-data', commentObject)
   }
 }
 
-const getreplyItemFromDb = async () => {
-  const replyData = await getEntryFromDb('reply-data')
-  const replyItems = replyData.reverse().map((replyItem) => {
-    const { replyItemId, userName,userPhoto, commentBoxValue } = replyItem
+const getCommentItemFromDb = async () => {
+  const commentData = await getEntryFromDb('comment-data')
+  const commentItems = commentData.reverse().map((commentItem) => {
+    const { commentItemId, userName,userPhoto, inputValue } = commentItem
     return `
-      <div class="reply-item" id="${replyItemId}">
-        <div class="reply-content-item">
+      <div class="comment-item" id="${commentItemId}">
+        <div class="comment-content-item">
           <img src="${userPhoto}" class="home-page-photo image" alt="photo">
           <div>
             <strong class="user-profile-name">${userName}</strong>
-            <p>${commentBoxValue}</p>
-            <div class="reply-item-options">
+            <p>${inputValue}</p>
+            <div class="comment-item-options">
               <button><i class="fa fa-comment-o"></i></button>
               <button><i class="fa fa-retweet"></i></button>
               <button><i class="fa fa-heart-o"></i></button>
@@ -71,31 +69,32 @@ const getreplyItemFromDb = async () => {
     `
   })
 
-  document.querySelector('#replyOutput').innerHTML = replyItems.join('')
+  document.querySelector('#commentOutput').innerHTML = commentItems.join('')
 }
 
-const addCommentPageEvents = async () => {
+const addCommentPageEvents = () => {
   const commentModal = document.querySelector('.comment-modal-container')
   const overlay = document.querySelector('#overlay')
   document.querySelector('#returnButton').addEventListener('click', () => {
     switchCurrentPage('homePage')
   })
 
-  document.querySelector('.comment-button').addEventListener('click', () => {
+  document.querySelector('.comment-modal-button').addEventListener('click', () => {
     commentModal.style.display = 'block'
     overlay.style.display = 'block'
   })
 
-  document.querySelector('.comment-modal-button').addEventListener('click', () => {
+  document.querySelector('.close-comment-button').addEventListener('click', () => {
     commentModal.style.display = 'none'
     overlay.style.display = 'none'
   })
 
-  document.querySelector('.add-reply-button').addEventListener('click', () => {
-    addreplyItemToDb()
+  document.querySelector('.add-comment-button').addEventListener('click', () => {
+    const tweetId =  document.querySelector('.add-comment-button').id
+    addcommentItemToDb(tweetId)
   })
 
-  getreplyItemFromDb()
+  getCommentItemFromDb()
 }
 
 export default addCommentPageEvents
