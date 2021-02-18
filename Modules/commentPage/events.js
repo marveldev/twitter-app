552d2006cead762ace61070c390330775758719e
Marvel Dev <marvellousalika48@gmail.com>
2021-02-18 08:13:36 +0100

    refactor file content

--- Modules/commentPage/events.js
+++ Modules/commentPage/events.js
@@ -1,22 +1,24 @@
 import { addEntryToDb, getEntryFromDb } from "../../dataStorage.js"
 import switchCurrentPage from "../helper.js"
 
-const addCommentItemToDb = () => {
+const addreplyItemToDb = () => {
   const replyOutput = document.querySelector('#replyOutput')
   const commentBox = document.querySelector('.comment-box')
 
   if (commentBox.value.trim().length >= 1) {
-    const commentItemId = 'id' + Date.parse(new Date()).toString()
+    const replyItemId = 'id' + Date.parse(new Date()).toString()
+    const userName = document.querySelector('.profile-name').innerText
+    const userPhoto = document.querySelector('.left-nav-photo').src
     const commentBoxValue = commentBox.value
-    let commentItem = `
-      <div class="comment-item" id="${commentItemId}">
-        <div class="comment-content-item">
-          <img src="" class="home-page-photo image" alt="photo">
+    let replyItem = `
+      <div class="reply-item" id="${replyItemId}">
+        <div class="reply-content-item">
+          <img src="${userPhoto}" class="home-page-photo image" alt="photo">
           <div>
-            <strong class="user-profile-name">Jane Doe</strong>
-            <p class="">Hey</p>
-            <div class="comment-item-options">
-              <button class="comment-button"><i class="fa fa-comment-o"></i></button>
+            <strong class="user-profile-name">${userName}</strong>
+            <p>${commentBoxValue}</p>
+            <div class="reply-item-options">
+              <button><i class="fa fa-comment-o"></i></button>
               <button><i class="fa fa-retweet"></i></button>
               <button><i class="fa fa-heart-o"></i></button>
               <button><i class="fa fa-upload"></i></button>
@@ -26,8 +28,8 @@ const addCommentItemToDb = () => {
         <button class="more-button" property=""><i class="material-icons">&#xe5d3;</i></button>
       </div>
     `
-    commentItem += replyOutput.innerHTML
-    replyOutput.innerHTML = commentItem
+    replyItem += replyOutput.innerHTML
+    replyOutput.innerHTML = replyItem
 
     commentBox.value = ''
 
@@ -35,27 +37,29 @@ const addCommentItemToDb = () => {
     document.querySelector('#overlay').style.display = 'none'
 
     const addItemToIndexDb = {
-      commentItemId: commentItemId,
+      replyItemId: replyItemId,
+      userName: userName,
+      userPhoto: userPhoto,
       commentBoxValue: commentBoxValue
     }
 
-    addEntryToDb('comment-data', addItemToIndexDb)
+    addEntryToDb('reply-data', addItemToIndexDb)
   }
 }
 
-const getCommentItemFromDb = async () => {
-  const commentData = await getEntryFromDb('comment-data')
-  const commentItems = commentData.reverse().map((commentItem) => {
-    const { commentItemId, commentBoxValue } = commentItem
+const getreplyItemFromDb = async () => {
+  const replyData = await getEntryFromDb('reply-data')
+  const replyItems = replyData.reverse().map((replyItem) => {
+    const { replyItemId, userName,userPhoto, commentBoxValue } = replyItem
     return `
-      <div class="comment-item" id="${commentItemId}">
-        <div class="comment-content-item">
-          <img src="" class="home-page-photo image" alt="photo">
+      <div class="reply-item" id="${replyItemId}">
+        <div class="reply-content-item">
+          <img src="${userPhoto}" class="home-page-photo image" alt="photo">
           <div>
-            <strong class="user-profile-name">Jane Doe</strong>
-            <p class="">Hey</p>
-            <div class="comment-item-options">
-              <button class="comment-button"><i class="fa fa-comment-o"></i></button>
+            <strong class="user-profile-name">${userName}</strong>
+            <p>${commentBoxValue}</p>
+            <div class="reply-item-options">
+              <button><i class="fa fa-comment-o"></i></button>
               <button><i class="fa fa-retweet"></i></button>
               <button><i class="fa fa-heart-o"></i></button>
               <button><i class="fa fa-upload"></i></button>
@@ -67,7 +71,7 @@ const getCommentItemFromDb = async () => {
     `
   })
 
-  document.querySelector('#replyOutput').innerHTML = commentItems.join('')
+  document.querySelector('#replyOutput').innerHTML = replyItems.join('')
 }
 
 const addCommentPageEvents = async () => {
@@ -77,30 +81,21 @@ const addCommentPageEvents = async () => {
     switchCurrentPage('homePage')
   })
 
-  const commentButtons = document.querySelectorAll('.comment-button')
-  commentButtons.forEach(commentButton => {
-    commentButton.addEventListener('click', () => {
-      commentModal.style.display = 'block'
-      overlay.style.display = 'block'
-    })
+  document.querySelector('.comment-button').addEventListener('click', () => {
+    commentModal.style.display = 'block'
+    overlay.style.display = 'block'
   })
 
-  const commentModalButtons = document.querySelectorAll('.comment-modal-button')
-  commentModalButtons.forEach(commentModalButton => {
-    commentModalButton.addEventListener('click', () => {
-      commentModal.style.display = 'none'
-      overlay.style.display = 'none'
-    })
+  document.querySelector('.comment-modal-button').addEventListener('click', () => {
+    commentModal.style.display = 'none'
+    overlay.style.display = 'none'
   })
 
-  const addCommentButtons = document.querySelectorAll('.add-comment-button')
-  addCommentButtons.forEach(addCommentButton => {
-    addCommentButton.addEventListener('click', () => {
-      addCommentItemToDb()
-    })
+  document.querySelector('.add-reply-button').addEventListener('click', () => {
+    addreplyItemToDb()
   })
 
-  getCommentItemFromDb()
+  getreplyItemFromDb()
 }
 
 export default addCommentPageEvents
